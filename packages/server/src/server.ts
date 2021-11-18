c96ae5efb6b725c8b54a89263ed63a0b4c9d5a8b
Paweł Chyła <chyla1988@gmail.com>
2021-11-18 01:11:45 +0100

    Eslint setup

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,12 +1,13 @@
-import express, { Request, Response } from "express";
+import type { Request, Response } from 'express';
+import express from 'express';
 
 const app = express();
 const PORT = 3003;
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({ greeting: "Hello world!" });
+app.get('/', (req: Request, res: Response) => {
+  res.json({ greeting: 'Hello world!' });
 });
 
 app.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
+  console.log('Server is running on port ' + PORT);
 });
