4730171939061d07c12ff288455b3909aebd3698
Paweł Chyła <chyla1988@gmail.com>
2022-01-26 22:18:29 +0100

    Add cookie parser

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import { errorHandler } from './middlewares/errorHandler';
 import { getRoutes } from './routes';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT || 3003;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use('/api', getRoutes());
 
